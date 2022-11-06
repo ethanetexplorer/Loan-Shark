f7493d7579aa1c722f9fd1bccb0a610b0848050e
Ethan Lim <ethanolicacid@outlook.com>
2022-11-06 14:37:53 +0800

    Initial Commit

---
+++ Loan Shark/Loan_SharkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loan_SharkApp.swift
+//  Loan Shark
+//
+//  Created by Ethan Lim on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Loan_SharkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
