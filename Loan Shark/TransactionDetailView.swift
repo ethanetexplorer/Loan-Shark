73b06c49217acf1b894ea2d345860c1d60a1eb9c
Ethan Lim <ethanolicacid@outlook.com>
2022-11-09 15:12:09 +0800

    Added contactsList in TransactionDetailView, may or may not be inspired by stations on the Circle Line
    Haha train go choo choo oh wait we don't use steam trains here in Singapore do we?

--- Loan Shark/TransactionDetailView.swift
+++ Loan Shark/TransactionDetailView.swift
@@ -10,9 +10,84 @@ import SwiftUI
 struct TransactionDetailView: View {
     
     @State var showBillSplit: Bool
+    @StateObject var manager = TransactionManager()
+    
+    @State var newTransaction = Transaction(name: "", people: [""], dueDate: "", isPaid: false, isBillSplitTransaction: false, money: 0)
+    let transactionTypes = ["Loan", "Bill Split"]
+    
+    @State var selectedContact = ""
+    var contacts = ["Dhoby Ghaut", "Bras Basah", "Esplanade", "Promenade", "Nicoll Highway", "Stadium", "Mountbatten", "Dakota", "Paya Lebar", "MacPherson", "Tai Seng", "Bartley", "Serangoon", "Lorong Chuan", "Bishan", "Marymount", "Caldecott", "Botanic Gardens", "Farrer Road", "Holland Village", "Buona Vista", "one-north", "Kent Ridge", "Haw Par Villa", "Pasir Panjang", "Labrador Park", "Telok Blangah", "HarbourFront", "Keppel", "Cantonment", "Prince Edward Road", "Marina Bay", "Bayfront"]
+    
+    var decimalNumberFormat: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.allowsFloats = true
+        numberFormatter.numberStyle = .currency
+        numberFormatter.currencySymbol = "$"
+        return numberFormatter
+    }
     
     var body: some View {
-        Text("Blah")
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("Transaction details")) {
+                        HStack{
+                            Text("Title")
+                            TextField("Add title", text: $0.name)
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        HStack{
+                            Text("People:")
+                            Picker ("People", selection: $selectedContact) {
+                                ForEach(contacts, id:\.self) {
+                                    Text($0)
+                                }
+                            }
+                        }
+                        HStack{
+                            Text("Amount of money:")
+                            TextField("Amount", value: $0.money, formatter: NumberFormatter())
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        DatePicker("Due by", selection: $0.dueDate, in: .now..., displayedComponents: .date)
+                        Picker("Transaction type", selection: $0.transactionType) {
+                            ForEach(transactionTypes, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                    }
+                    Section(header: Text("Options")) {
+                        Toggle(isOn: $0.isDetailSyncronised) {
+                            Text("Syncronise details")
+                        }
+                    }
+                }
+                Button{
+                    print("Saved transaction")
+                } label: {
+                    Text("Save")
+                        .frame(height: 50)
+                        .frame(maxWidth: .infinity)
+                        .background(.blue)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal)
+            }
+            Button{
+                print("Saved transaction")
+            } label: {
+                Text("Save")
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+            }
+            .navigationTitle("Details")
+        }
     }
 }
 
